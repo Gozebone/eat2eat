4e9e81283ea543f00049c749c0949f9d56d69eab
Gozebone <gozebone@gmail.com>
2022-07-18 10:37:34 +0300

    DB configured

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=50)
+    img   = models.ImageField(upload_to='products/')
