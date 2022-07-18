ce22767a23460743fe5639a66070f0f1629752a0
Nikolay Vasilev <gozebone@gmail.com>
2022-07-18 11:37:58 +0300

    products on home page

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from ..products.models import Product
 # Create your views here.
 
+
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html")
+    context = {
+        'products': Product.objects.all(),
+    }
+    return render(request, "home.html", context)
 
