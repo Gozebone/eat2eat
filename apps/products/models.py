1b16e5abdfe5ea24d7b8cadd5c64806f3e6f30c9
Nikolay Vasilev <gozebone@gmail.com>
2022-07-19 11:32:35 +0300

    root changes

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2, max_digits=50)
-    img   = models.ImageField(upload_to='products/')
+    img = models.ImageField(upload_to='products/')
--- estpoest/settings.py
+++ estpoest/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 
 # Media Files
-MEDIA_ROOT = BASE_DIR / 'media/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
