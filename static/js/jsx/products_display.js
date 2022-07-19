6ccfcc00b462a7d787070dde6114e20a7de0fa86
Nikolay Vasilev <gozebone@gmail.com>
2022-07-19 09:24:16 +0300

    safe flag

--- static/js/jsx/products_display.js
+++ static/js/jsx/products_display.js
@@ -8,13 +8,13 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      console.log(products.title);
-      return products.title;
+      console.log(products[0].title);
+      return products[0].title;
     }
 
     return (
       <button onClick={() => this.setState({ liked: true })}>
-        {products.title}
+        {products[0].title}
       </button>
     );
   }
