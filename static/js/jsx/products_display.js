868e2843d6ef27bb7d9dc81564ba7d0257fbfb4f
Nikolay Vasilev <gozebone@gmail.com>
2022-07-19 10:51:33 +0300

    img test

--- static/js/jsx/products_display.js
+++ static/js/jsx/products_display.js
@@ -3,18 +3,12 @@
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
   }
 
   render() {
-    if (this.state.liked) {
-      console.log(products);
-      return products;
-    }
-
     return products.map((item, index) => (
       <div className="product" key={index}>
-        {item.fields.title}
+        {item.fields.img}
         {item.fields.price}
         {item.fields.title}
       </div>
