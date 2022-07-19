7d965664f686d9caba3ba797d40f733310e4fdcd
Nikolay Vasilev <gozebone@gmail.com>
2022-07-19 08:42:04 +0300

    first try part 2

--- static/js/jsx/products_display.js
+++ static/js/jsx/products_display.js
@@ -13,7 +13,7 @@ class LikeButton extends React.Component {
 
     return (
       <button onClick={() => this.setState({ liked: true })}>
-        Like
+        products
       </button>
     );
   }
