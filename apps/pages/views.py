83acdbeefafa943e39d4f6f7ea3d7cdebe761451
Nikolay Vasilev <gozebone@gmail.com>
2022-07-19 10:09:40 +0300

    queryset to json

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,4 @@
-import json
+from django.core import serializers
 from django.shortcuts import render
 from ..products.models import Product
 # Create your views here.
@@ -6,7 +6,7 @@ from ..products.models import Product
 
 def home_view(request, *args, **kwargs):
     context = {
-        'products': json.dumps(list(Product.objects.all())),
+        'products': serializers.serialize("json", Product.objects.all()),
     }
     return render(request, "home.html", context)
 
