c22346d2a063698899b7731a963f455aab54f961
Nikolay Vasilev <gozebone@gmail.com>
2022-07-21 20:48:11 +0300

    test

--- static/js/jsx/products_display.js
+++ static/js/jsx/products_display.js
@@ -6,25 +6,26 @@ for (let el in products){
         products_separate[el.type] = [];
     }
      products_separate[el.type].push(el);
+    console.log(products_separate)
 }
 
-class MenuItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return products_separate['SA'].map((item, index) => (
-      <div className="product" key={index}>
-        <img src={"/media/" + item.fields.img} alt={item.fields.title} />
-        <p>{item.fields.title}</p>
-        <p>{item.fields.composition}</p>
-        <p>{item.fields.price}</p>
-      </div>
-    ));
-  }
-}
-
-const domContainer = document.querySelector('#menu');
-const root = ReactDOM.createRoot(domContainer);
-root.render(React.createElement(MenuItem));
+// class MenuItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render() {
+//     return products.map((item, index) => (
+//       <div className="product" key={index}>
+//         <img src={"/media/" + item.fields.img} alt={item.fields.title} />
+//         <p>{item.fields.title}</p>
+//         <p>{item.fields.composition}</p>
+//         <p>{item.fields.price}</p>
+//       </div>
+//     ));
+//   }
+// }
+//
+// const domContainer = document.querySelector('#menu');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(React.createElement(MenuItem));
