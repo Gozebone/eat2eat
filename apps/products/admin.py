409ae2f02a3b6bb2d279ac54fddda28d9dec2265
Nikolay Vasilev <gozebone@gmail.com>
2022-07-21 16:45:57 +0300

    Revert "product admin class"
    This reverts commit cc0a4d72f95696b8a85dc2e03cdea3b8ac84ec64.

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = list(vars(Product).keys())
-
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Product)
