06231385a9b2c8d7686455ea1096dc55e9dc9407
Nikolay Vasilev <gozebone@gmail.com>
2022-07-23 12:10:19 +0300

    json module

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -2,13 +2,14 @@ from django.core import serializers
 from django.shortcuts import render
 from ..products.models import Product
 from ..products.choices import *
+import json
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
     context = {
         'products': serializers.serialize("json", Product.objects.all()),
-        'products_labels': serializers.serialize("json", TYPE_CHOICES),
+        'products_labels': json.dumps(dict(TYPE_CHOICES)),
     }
     return render(request, "home.html", context)
 
