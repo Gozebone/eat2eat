ac61c35854ece7163cbc042edcf0d57cd1d80e69
Nikolay Vasilev <gozebone@gmail.com>
2022-07-23 16:42:40 +0300

    trying multiple blocks in return

--- static/js/jsx/products_display.js
+++ static/js/jsx/products_display.js
@@ -5,25 +5,31 @@ for (let el in products){
     if (typeof products_separate[products[el].fields.type] === 'undefined'){
         products_separate[products[el].fields.type] = [];
     }
-     products_separate[products[el].fields.type].push(products[el]);
-    console.log(products_labels['SA'])
+    products_separate[products[el].fields.type].push(products[el]);
+    console.log(products_labels)
 }
 
 class MenuItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return products_separate['SA'].map((item, index) => (
-      <div className="product" key={index}>
-        <img src={"/media/" + item.fields.img} alt={item.fields.title} />
-        <p>{item.fields.title}</p>
-        <p>{item.fields.composition}</p>
-        <p>{item.fields.price}</p>
-      </div>
-    ));
-  }
+    render() {
+        return products_labels.map((products_label) => {
+            <React.Fragment id={products_label}>
+            {
+                products_separate['SA'].map((item, index) => (
+                    <React.Fragment className="product" key={index}>
+                        <img src={"/media/" + item.fields.img} alt={item.fields.title}/>
+                        <p>{item.fields.title}</p>
+                        <p>{item.fields.composition}</p>
+                        <p>{item.fields.price}</p>
+                    </React.Fragment>
+                ))
+            }
+            </React.Fragment>
+        });
+    }
 }
 
 const domContainer = document.querySelector('#menu');
