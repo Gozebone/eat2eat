338e9cc2360b709bea2db1cbca4b0f1b8b82c283
Nikolay Vasilev <gozebone@gmail.com>
2022-07-27 18:56:09 +0300

    320:240

--- apps/products/models.py
+++ apps/products/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     title = models.CharField(max_length=30)
     price = models.DecimalField(decimal_places=2, max_digits=50)
     img = ProcessedImageField(upload_to='products/',
-                              processors=[ResizeToFill(300,150)],
+                              processors=[ResizeToFill(320,240)],
                               format='JPEG',
                               options={'quality': 60})
     type = models.CharField(max_length=2, choices=TYPE_CHOICES)
